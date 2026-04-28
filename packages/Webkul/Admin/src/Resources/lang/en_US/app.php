d52b9f86bc388f7b7170e1c45f6839629a163205
Johannes Rudolph <johannes.rudolph@dia-software.de>
2026-04-28 12:13:12 +0200

    Add appearance settings page for admin logo and favicon

--- packages/Webkul/Admin/src/Resources/lang/en_US/app.php
+++ packages/Webkul/Admin/src/Resources/lang/en_US/app.php
@@ -1721,6 +1721,19 @@ return [
             'current-user-delete-error' => 'The logged in user can not be deleted.',
         ],
 
+        'appearance' => [
+            'title'          => 'Appearance',
+            'section-title'  => 'Visual Branding',
+            'logo'           => 'Logo',
+            'logo-size'      => 'Recommended image resolution: 192px X 50px',
+            'favicon'        => 'Favicon',
+            'favicon-size'   => 'Recommended image resolution: 16px X 16px',
+            'no-logo'        => 'No logo uploaded yet.',
+            'no-favicon'     => 'No favicon uploaded yet.',
+            'save-btn'       => 'Save Appearance',
+            'update-success' => 'Appearance updated successfully.',
+        ],
+
         'roles' => [
             'index' => [
                 'create-btn' => 'Create Role',
@@ -2115,6 +2128,7 @@ return [
                 'products'           => 'Products',
                 'roles'              => 'Roles',
                 'settings'           => 'Settings',
+                'appearance'         => 'Appearance',
                 'themes'             => 'Themes',
                 'users'              => 'Users',
                 'integrations'       => 'Integrations',
@@ -2395,6 +2409,7 @@ return [
         'reporting'                => 'Reporting',
         'reviews'                  => 'Reviews',
         'roles'                    => 'Roles',
+        'appearance'               => 'Appearance',
         'sales'                    => 'Sales',
         'search-seo'               => 'Search & SEO',
         'search-synonyms'          => 'Search Synonyms',
